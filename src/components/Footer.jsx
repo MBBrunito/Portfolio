abd22592675fa2bed97b3c98283fa68949d64788
MBBrunito <tecnolastipas@gmail.com>
2023-10-15 19:15:12 -0300

    Footbar component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { contact } from "../../profile";
+import "./Footer.css";
+
+export default function Footer() {
+   return (
+      <footer>
+         <div className="footer">
+            <div>
+               <img
+                  src="https://res.cloudinary.com/daoavxvau/image/upload/v1697171874/MBDevFS_vt13er.png"
+                  alt=""
+               />
+            </div>
+            <div>
+               <h3>&copy; MarcosBrunoDev</h3>
+               <p>2023 - {new Date().getFullYear()}</p>
+               <p>All rights Reserved.</p>
+            </div>
+            <div className="redes">
+               {contact.map((cont, index) => (
+                  <div key={index}>
+                     <Link href={cont.contact} target="_blank">
+                        <img src={cont.logo} alt="" />
+                     </Link>
+                  </div>
+               ))}
+            </div>
+         </div>
+      </footer>
+   );
+}
